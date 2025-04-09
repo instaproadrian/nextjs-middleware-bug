857efe9bdf5b7d30cdddb7c2b6dcb770d48c5878
Adrian Osmond <adrian.osmond@instapro.com>
2025-04-09 17:41:00 +0200

    Create 2 pages, Introduce middleware

---
+++ src/pages/page2.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import type { FC } from "react";
+
+const Page2: FC = () => (
+  <>
+    <h1>Page 2</h1>
+    <Link href="/">Home</Link>
+  </>
+);
+
+export default Page2;
